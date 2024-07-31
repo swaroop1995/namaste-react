989afe9647ae7a718672b4939d2552f2a122ffd4
swaroop1995 <swaroop040@gmail.com>
2024-07-31 11:19:21 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,18 @@
+//<div id="parent">
+//  <div id="child">
+//    <h1> I am react child <h1>
+// </div>
+// </div>
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am react parent"),
+    React.createElement("h2", {}, "I am react child"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am react parent"),
+    React.createElement("h2", {}, "I am react child"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);

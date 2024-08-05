227c7afb1e1f9f9a59e7b7164f7b3f7cfc02df14
swaroop1995 <swaroop040@gmail.com>
2024-08-05 12:06:43 +0530

    episode 2 and episode 3 day 01

--- App.js
+++ App.js
@@ -2,14 +2,53 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // JSX=>React.createElement()=>ReactElement-JS Object=>HTMLRender(render)
 // React element
-const heading = (
-  <h1 id="heading" className="head" tabIndex={1}>
-    Namaste React using JSX
-  </h1>
-);
+
 // React functional component
-const HeadingComponent = () => {
-  return <h1>Namaste React functional component</h1>;
+// Component composition
+
+/*Planning food app
+header 
+ -logo
+ -nav items
+Body  
+ -search bar
+ -restaurant container
+ -restaurant card
+Footer
+ -copyright
+ -Links
+ -address
+ -contact
+ */
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<AppLayout />);
